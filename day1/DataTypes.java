f36b930299567375b424a65697e5e6fd795446f7
Learnselenium-d <dyson.antho@gmail.com>
2024-07-27 13:59:40 +0530

    Add files via upload
    Day-1

---
+++ day1/DataTypes.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float browserversion =100.2f;
+		String browsername ="firefox";
+		boolean isvisible =true ;
+		int releaseyear =1998;
+		char browserlogo ='f';
+		System.out.println("The browser version:"+browserversion+"\n"+"browser name:"+browsername);
+		System.out.println("The browser name visible:"+isvisible+"\n"+"browser released Year:"+releaseyear);
+		System.out.println("The browser logo:"+browserlogo);
+	}
+
+}
